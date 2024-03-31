d548da3e551d01b3b5509c36cc6392fe36a4454f
DopamineNone <123919976+DopamineNone@users.noreply.github.com>
2024-03-31 20:04:03 +0800

    feat client

---
+++ client/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	ue "_5gAKA_go/kitex_gen/_5gAKA_go/UE/protocolservice"
+	"context"
+	"fmt"
+)
+
+func main() {
+	cli, err := ue.NewClient("_5gAKA_go.UE")
+	if err != nil {
+		fmt.Println("Error occurred: ", err.Error())
+		return
+	}
+	err = cli.Authenticate(context.Background())
+	if err == nil {
+		fmt.Println("Authenticate successfully!")
+	} else {
+		fmt.Println("Error occurred: ", err.Error())
+	}
+}
