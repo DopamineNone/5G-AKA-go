56e1f7dfa96e1543969b2bddcf22703c90ae7848
DopamineNone <123919976+DopamineNone@users.noreply.github.com>
2024-04-01 03:52:04 +0800

    perf log and error
    - Add multiwriter to log new info
    - Add error chain and restrict authenticate return value

--- rpc/SEAF/handler.go
+++ rpc/SEAF/handler.go
@@ -63,12 +63,13 @@ func (s *ProtocolServiceImpl) Authenticate(ctx context.Context, data string) (re
 		if hResStar == hxResStar {
 			log.Println(time.Now().Format("2006-01-02 15:04:05") + "  " + "SEAF Authentication Passed.")
 
-			resp, err := ausfClient.Authenticate(context.Background(), resStar)
+			_, err := ausfClient.Authenticate(context.Background(), resStar)
 			log.Println(time.Now().Format("2006-01-02 15:04:05") + "  " + "Send res* to AUSF.")
 			if err != nil {
-				log.Println(time.Now().Format("2006-01-02 15:04:05") + "  " + resp)
-				return resp, fmt.Errorf(resp)
+				log.Println(time.Now().Format("2006-01-02 15:04:05") + "  " + err.Error())
+				return "", err
 			}
+			return "Authentication passed successfully!", nil
 		} else {
 			msg := "SEAF Authentication failed: Unable to pass access-auth-check"
 			log.Println(time.Now().Format("2006-01-02 15:04:05") + "  " + msg)
